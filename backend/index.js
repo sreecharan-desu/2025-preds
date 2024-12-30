804704bcaa0dbc16355702e8f2f5bb723193567b
SreeCharan <sreecharan309@gmail.com>
2024-12-30 20:05:58 +0530

    prod

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    res.send("Hello from backend.")
+})
+
 app.post('/post',async(req,res)=>{
     const {name,data} = req.body;
     // console.log()
